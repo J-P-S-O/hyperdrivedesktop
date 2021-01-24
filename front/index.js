d086eef6457358180e3982db0caf0df66bfbdf3b
J-p-s-o <jpso.guerreiro@gmail.com>
2021-01-24 16:39:10 -0300

    .

--- front/index.js
+++ front/index.js
@@ -3,8 +3,8 @@ const Hyperdrive = require("Hyperdrive")
 onload = () =>{
   let bd = document.querySelector("body")
   if (location.hash.startsWith("hyper://")){
-    
     //process hash
+
   }else{
 
   }
